52d6d7d0009327f876fa9c6554663efba1cb7a4b
takumi091111 <takumi091111@gmail.com>
2019-09-18 18:29:50 +0000

    スマホ向けヘッダーの実装

---
+++ components/MobileHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mobile-header">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend()
+</script>
+
+<style lang="postcss" scoped>
+.mobile-header {
+  display: none;
+  justify-content: space-between;
+  @media (max-width: 640px) {
+    display: flex;
+  }
+}
+</style>
---
+++ components/MobileHeaderButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="mobile-header-button" :aria-label="label" @click="onClick">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('click')
+    }
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+.mobile-header-button {
+  width: 50px;
+  height: 50px;
+}
+</style>
