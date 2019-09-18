205fcfec0bf2456d2fb7520321f34c5a3dbac2ad
takumi091111 <takumi091111@gmail.com>
2019-09-18 17:09:13 +0000

    ルートの追加

---
+++ assets/data/routes.ts
@@ -0,0 +1,7 @@
+import { Route } from '~/assets/interfaces/Route'
+
+export default [
+  { name: 'HOME', path: '/' },
+  { name: 'BLOG', path: '/blog' },
+  { name: 'ABOUT', path: '/about' }
+] as Route[]
