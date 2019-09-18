793ef6aad72f71ce3677d5dabdc018d1901fed51
takumi091111 <takumi091111@gmail.com>
2019-09-18 22:08:42 +0000

    アイコンの実装

---
+++ components/MenuIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <img svg-inline src="~/assets/images/menu.svg" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend()
+</script>
---
+++ components/ThemeIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <img svg-inline src="~/assets/images/theme.svg" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend()
+</script>
