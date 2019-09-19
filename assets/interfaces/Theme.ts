aee6d525cf341fca2011b16ff9d743f182493cc0
takumi091111 <takumi091111@gmail.com>
2019-09-19 03:59:51 +0000

    テーマの追加

---
+++ assets/interfaces/Theme.ts
@@ -0,0 +1,20 @@
+interface Theme {
+  '--black': string
+  '--white': string
+  '--gray': string
+  '--deep-gray': string
+  '--font-color': string
+  '--font-hover-color': string
+  '--background-color': string
+  '--background-hover-color': string
+  '--border-color': string
+  '--border-hover-color': string
+  '--code-block-font-color': string
+  '--code-block-background-color': string
+}
+
+export interface Themes {
+  [key: string]: Theme
+  light: Theme
+  dark: Theme
+}
